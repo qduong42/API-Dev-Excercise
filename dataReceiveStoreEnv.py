38567d521e352d6c8d93498a4bfea71535f7d191
acer42 <qduong@students.42wolfsburg.de>
2024-02-06 14:05:50 +0100

    unique key dup key error when running twice

--- dataReceiveStoreEnv.py
+++ dataReceiveStoreEnv.py
@@ -1,4 +1,5 @@
 import requests
+import pymongo
 from pymongo import MongoClient
 from dotenv import dotenv_values  # So we do not need to work with os env values
 
@@ -14,15 +15,18 @@ db = client["jsonplaceholder"]
 def insert_data_from_url(collection, url):
     response = requests.get(url)
     data = response.json()
-    for item in data:
-        query = {key: item[key] for key in item if key != 'id'}
-        existing_document = collection.find_one(query)
-        if not existing_document:
-            collection.insert_one(item)
+    # for item in data:
+    #     query = {key: item[key] for key in item if key not in('id')}
+    #     existing_document = collection.find_one(query)
+    #     if not existing_document:
+    #         collection.insert_one(item)
+    collection.insert_many(data)
 
 
 collection_posts = db["posts"]
+collection_posts.create_index([('id', pymongo.ASCENDING)], unique=True)
 collection_comments = db["comments"]
+collection_comments.create_index([('id', pymongo.ASCENDING)], unique=True)
 insert_data_from_url(collection_posts, "https://jsonplaceholder.typicode.com/posts")
 insert_data_from_url(collection_comments, "https://jsonplaceholder.typicode.com/comments")
 collection_comments.update_many({}, {"$rename": {"postId": "userId"}})
