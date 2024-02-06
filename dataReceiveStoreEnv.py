9683c279b58bf608b5d1cb98b17619ce0b204bad
acer42 <qduong@students.42wolfsburg.de>
2024-02-06 14:06:32 +0100

    removed comments for prev code to try to avoid dupl

--- dataReceiveStoreEnv.py
+++ dataReceiveStoreEnv.py
@@ -15,11 +15,6 @@ db = client["jsonplaceholder"]
 def insert_data_from_url(collection, url):
     response = requests.get(url)
     data = response.json()
-    # for item in data:
-    #     query = {key: item[key] for key in item if key not in('id')}
-    #     existing_document = collection.find_one(query)
-    #     if not existing_document:
-    #         collection.insert_one(item)
     collection.insert_many(data)
 
 
