9a0bc793a85026621f19ab7ac2dcfc86e9a78ae2
acer42 <qduong@students.42wolfsburg.de>
2024-02-06 14:26:40 +0100

    added a check to only insert if some fields are unique,  avoids errors when executing this file twice

--- dataReceiveStoreEnv.py
+++ dataReceiveStoreEnv.py
@@ -12,16 +12,23 @@ client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@127.0.0.1:270
 db = client["jsonplaceholder"]
 
 
-def insert_data_from_url(collection, url):
+def insert_data_from_url(collection, url, unique_fields):
     response = requests.get(url)
     data = response.json()
-    collection.insert_many(data)
+    for item in data:
+        unique_values = {key: item[key] for key in unique_fields}
+        existing_document = collection.find_one(unique_values)
+        if not existing_document:
+            collection.insert_one(item)
 
 
 collection_posts = db["posts"]
 collection_posts.create_index([('id', pymongo.ASCENDING)], unique=True)
+unique_posts_fields = ["userId", "title", "body"]
+insert_data_from_url(collection_posts, "https://jsonplaceholder.typicode.com/posts", unique_posts_fields)
+
 collection_comments = db["comments"]
 collection_comments.create_index([('id', pymongo.ASCENDING)], unique=True)
-insert_data_from_url(collection_posts, "https://jsonplaceholder.typicode.com/posts")
-insert_data_from_url(collection_comments, "https://jsonplaceholder.typicode.com/comments")
+unique_comments_fields = ["name", "email", "body"]
+insert_data_from_url(collection_comments, "https://jsonplaceholder.typicode.com/comments", unique_comments_fields)
 collection_comments.update_many({}, {"$rename": {"postId": "userId"}})
