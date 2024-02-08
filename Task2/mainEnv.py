61221a7dd407359d28d7bec40cc2f64668791ec3
qduong42 <qduong@students.42berlin.de>
2024-02-08 21:50:46 +0100

    added optional queries, limit when no start/end time

--- Task2/mainEnv.py
+++ Task2/mainEnv.py
@@ -6,9 +6,7 @@ from datetime import datetime
 from typing import Optional
 
 from json import dumps, JSONDecodeError
-import logging
 
-logging.basicConfig(level=logging.ERROR)
 config = dotenv_values(".env")
 mongo_username = config.get("MONGO_INITDB_ROOT_USERNAME")
 mongo_password = config.get("MONGO_INITDB_ROOT_PASSWORD")
@@ -20,33 +18,31 @@ db = client["task2"]  # Replace with your database name
 collection_name = "turbine_data"
 collection = db[collection_name]
 
-# @app.get("/turbine_data/")
-# def get_turbine_data(
-#     turbine_id: int = Query(None, title="Turbine ID"),
-#     start_time: datetime = Query(None, title="Start Time"),
-#     end_time: datetime = Query(None, title="End Time")
-# ):
-#     query = {}
-#     if turbine_id is not None:
-#         query["turbine_id"] = turbine_id
-#     if start_time is not None and end_time is not None:
-#         query["timestamp"] = {"$gte": start_time, "$lte": end_time}
-    
-#     results = collection.find(query, {"_id": 0}).sort("timestamp")
-#     return list(results)
-
-# from fastapi import FastAPI, Query
-# from datetime import datetime
-# from typing import Optional
-# from pymongo import MongoClient
 @app.get("/")
 async def root():
   return {"available endpoint":"/turbine_data"}
 
 @app.get("/turbine_data")
-async def get_turbine_data(turbine_id: int):
-    query = {"turbine_id": turbine_id}
-    documents = list(collection.find(query, projection={"_id": False}).skip(1).limit(50))
+async def get_turbine_data(turbine_id: Optional[int] = Query(None), start_time: Optional[str] = Query(None), end_time: Optional[str] = Query(None)):
+    if start_time is not None:
+        start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
+        # "2016-02-01T00:50:00"
+    if end_time is not None:
+        end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M")
+    
+    if start_time is None or end_time is None:
+        limit = 1000
+    else:
+        limit = 0
+    
+    query = {}
+    if turbine_id is not None:
+        query = {"turbine_id": turbine_id}
+    if start_time is not None:
+        query["Dat/Zeit"] = {"$gte": start_time}
+    if end_time is not None:
+        query.setdefault("Dat/Zeit", {}).update({"$lte": end_time})
+    documents = list(collection.find(query, projection={"_id": False}).skip(1).limit(limit))
     return documents
 
 if __name__ == "__main__":
